3e29c70a76f0c8a8eabce178726ea36ecb05d1fd
RomanSoviak <romansoviak@gmail.com>
2025-07-20 21:32:44 +0300

    Create empty page with red background

---
+++ lib/app_page.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: MyHomePage());
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(color: Colors.red);
+  }
+}
