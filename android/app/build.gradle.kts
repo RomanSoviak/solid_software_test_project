991d82bd3786703a64a3292ba1e3f82262ab9c25
RomanSoviak <romansoviak@gmail.com>
2025-07-20 23:37:55 +0300

    Set ndkVersion = "27.0.12077973", cause of SP Flutter lib

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.solidsoftware.testproject.solid_software_test_project"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
